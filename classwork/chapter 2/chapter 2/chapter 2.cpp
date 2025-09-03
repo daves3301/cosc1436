87c0b6eb0912197d5ea24107d0d88447f7b56c4d
David Petroff <david.petroff@my.tccd.edu>
2025-09-02 21:31:26 -0500

    variables

---
+++ classwork/chapter 2/chapter 2/chapter 2.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << " david petroff" << std::endl;
+    std::cout << " you are " << 20 << " years old" << std::endl;
+    // if you want double quotes in string literal you have to escape them;
+    std::string name;
+    name = "dave";
+    std::cout << "hello " << name; 
+}
