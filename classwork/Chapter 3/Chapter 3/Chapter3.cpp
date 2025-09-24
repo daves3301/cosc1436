d2a10add494899d192ca8224b66d69028d8a80f7
David Petroff <david.petroff@my.tccd.edu>
2025-09-23 19:17:19 -0500

    lab1

---
+++ classwork/Chapter 3/Chapter 3/Chapter3.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+void main()
+{
+    std::cout << "enter values for x and y: ";
+    double x, y;
+    std::cin >> x >> y;
+    
+    std::cout << "Function" << "x" << "y" << "Result" << std::endl;
+    std::cout << "-------------------------------------" << std::endl;
+
+    std::cout << "pow " << x << y << pow(x, y) << std::endl;
+    std::cout << "sqrt " << x << y << sqrt(x, y) << std::endl;
+   
+    std::cout << "ceil " << x << y << ceil(x, y) << std::endl;
+    std::cout << "floor " << x << y << floor(x, y) << std::endl;
+
+    std::cout << "round " << x << y << round(x, y) << std::endl;
+    std::cout << "trunc " << x << y << trunc(x, y) << std::endl;
+}
+void ExpressionDemo()
+{    
+//*this code is not perfect be weary when studying, it has some sloppiness
+   
+    //promt user for radius of circle.
+    std::cout << "Enter radius";
+
+    //cin is used for input from user.
+    int radius = 0;
+    std::cin >> radius;
+
+    
+    //area = pi * r^2
+    const double Pi = 3.14159;
+    double area = Pi * radius * radius; 
+    std::cout << "Ares = " << Pi << "*" << radius << "*" << radius << std::endl;
+    std::cout << "Area = " << area << std::endl;
+
+
+    //triangle 
+    //area = 1/2 base * height
+
+    std::cout << "Enter base and height of triangle";
+    int base = 0, height = 0;
+    std::cin >> base;
+    std::cin >> height;
+    
+    
+    area = (1 / 2) * base * height; 
+    std::cout << "Area = " << area << std::endl;
+    
+    std::cout << "Enter name"; 
+    //getline reads strings with spaces in them line and puts it into variable.
+    //std::cin.ignore(), will throw out anything in the input buffer.
+    std::string name;
+    std::getline(std::cin, name);
+    std::cin >> name;
+    std::cout << name << std::endl;
+
+
+    //overflow and underflow 
+    short smallValue = 32767;
+    smallValue = smallValue + 1;
+    std::cout << smallValue << std::endl;
+
+    short largeValue = -32768;
+    largeValue = largeValue - 1;
+
+    std::cout << smallValue << " " << largeValue << std::endl;
+
+    //type coersion compiler implicitly converts an E to another type.doesnt care about how your going to use the type of the result (it is right associative).
+    //type casting : when you explicitly covert an expressions type to another type.
+    int left = 10;
+    int right = 3;
+    int intDivision = 10 / 3; //gives you an int
+    //want a fraction? make one of the numbers a double or .0 on the end 
+    double intDivison = 10 / 3.0; // gives you double
+    //if your dealing with variables, go off how you expect to STORE IN THE VARIABLE. never change the type based on the outcome you want. 
+    //type casting:
+    intDivision = (double) left / right; // change the type proceeding your variable. THIS IS CALLED A "C-CAST." either side it doesnt matter.
+    //alternate approach:
+    static_cast<double>(left) / right; //both will have same effect.
+
+    //setting percise decimal places for your outputs or anything else use std::setprecision,and std::fixed, THIS REQUIRES #INCLUDE <IOMANIP>
+
+
+    //to include higher level math functions USE #inlcude <cmath>
+    //math functions
+    // pow(x, y) = power X to the Y power: the number then the power you want to raise it to. 
+
+
+    /*
+    YES OR NO QUESTIONS REQUIRE #include <cctype>, then use a do while loop.
+    //
+    MANIPULATORS:
+    
+    can use std::setw(?) to determine a number of spaces in an output
+    you can also left or right align your setw outputs, proceed your functions with std::left or right, once applied they remain in place unless switched.
+    std::setfill --padding value (persistent), can be used for hiding passwords 
+    VALUE PERCISION:
+    USE SET PERCISION- will format each of your floating point values to that percision.(std::setpercision(?))
+    */
+}
--- labs/D.P. COSC 1436 LAB 1/D.P. COSC 1436 LAB 1/D.P. COSC 1436 LAB 1.cpp
+++ labs/D.P. COSC 1436 LAB 1/D.P. COSC 1436 LAB 1/D.P. COSC 1436 LAB 1.cpp
@@ -7,14 +7,19 @@ COSC 1436 FALL 2025
 
 #include <iostream>
 using namespace std;
-#include <stdlib.h>
+#include <string>
+#include <iomanip>
+#include <cctype>
 
 int main()
 {
-    string name;
-    cout << "please enter your name:";
-    cin >> int name;
-    int Lab1, Lab2, Lab3, Lab4;
+   
+    cout << "please enter your name:";   //requesting name from user then storing it.
+    string studentName;
+    
+    std::getline(cin, studentName);
+
+    double Lab1, Lab2, Lab3, Lab4;                     //requesting lab grades then storing them.
     cout << "please enter your grade for Lab 1:";
     cin >> Lab1;
     cout << "please enter your grade for Lab 2:";
@@ -24,47 +29,53 @@ int main()
     cout << "please enter your grade for Lab 4:";
     cin >> Lab4;
     
-    int Exam1, Exam2, Exam3, Exam4;
+    double Exam1, Exam2, Exam3;                 //requesting exam values the storing them.
     cout << "please enter your grade for Exam 1:";
     cin >> Exam1;
     cout << "please enter your grade for Exam 2:";
     cin >> Exam2;
     cout << "please enter your grade for Exam 3:";
     cin >> Exam3;
-    cout << "please enter your grade for Exam 4:";
-    cin >> Exam4;
     
-    int part
+    double Participation;                                          //requesting participation grade.
         std::cout << "please enter your participation grade:";
-    cin >> part;
+    cin >> Participation;
     
-    int Final;
+    double Final;
     cout << "Please enter your Final Exam grade:";
-    cin >> Final; 
+    cin >> Final;                                               //requesting final grade.
 
-    cout << name << " Your Lab grades are:";
-    cout << Lab1;
-    cout << Lab2;
-    cout << Lab3;
-    cout << Lab4;
+    cout << studentName << " Your Lab grades are:" << endl;             //outputting lab grades.
+    cout << "Lab 1: " <<Lab1 << endl;
+    cout << "Lab 2: " <<Lab2 << endl;
+    cout << "Lab 3: " <<Lab3 << endl;
+    cout << "Lab 4: " <<Lab4 << endl;
 
-    cout << name << "Your Exam grades are:";
-    cout << Exam1;
-    cout << Exam2;
-    cout << Exam3;
-    cout << Exam4;
+    cout << studentName << " Your Exam grades are:" << endl;            //outputting exam grades
+    cout << "Exam 1: " <<Exam1 << endl;
+    cout << "Exam 2: " <<Exam2 << endl;
+    cout << "Exam 3: " <<Exam3 << endl;
 
-    cout << name << "Your other grades are:";
-    cout << "Participation: " << part;
-    cout << "Final Exam: " << Final;
-    float LabAvg, ExamAvg, ClassAvg;
-    LabAvg = Lab1, Lab2, Lab3, Lab4 / 4;
-    ExamAvg = Exam1, Exam2, Exam3, Exam4 / 4;
-    ClassAvg = 
-        cout << name << ", Your class grade is: ";
-        cout << "Lab Average (65%): " << LabAvg;
-        cout << "Exam Average (20%): " << ExamAvg;
-        cout << "Participation (5%): " << part;
-        cout << "Class Average: " << ClassAvg;
+    cout << studentName << " Your other grades are:" << endl;
+    cout << "Participation: " << Participation << endl;        //outputting other grades.
+    cout << "Final Exam: " << Final << endl;
+    
+    double LabAvg, ExamAvg, ClassAvg;                           //declaring avg variables
+   
+    LabAvg = (Lab1 + Lab2 + Lab3 + Lab4) / 4;                        //calculating averages.
+    
+    ExamAvg = (Exam1 + Exam2 + Exam3) / 3;
+    
+    ClassAvg = (LabAvg * .65)+(ExamAvg * .20)+(Participation * .05)+(Final * .10) / 4;
+        
+    cout << studentName << ", Your class grades are: " << endl; //outputting student grades.
+    cout << std::fixed << std::setprecision(2);
+    cout << "Lab Average (65%): " << LabAvg << "%" << endl;
+    cout << "Exam Average (20%): " << ExamAvg << "%" << endl;
+    cout << "Participation (5%): " << Participation << "%" << endl;
+    cout << "Final Exam (10%): " << Final << "%" << endl;
+    cout << "Class Average: " << ClassAvg << "%" << endl;
 
+    
+        return 0;
 }
