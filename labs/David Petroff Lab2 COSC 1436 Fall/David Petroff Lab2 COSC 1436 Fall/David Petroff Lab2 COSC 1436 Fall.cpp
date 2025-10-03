e86561b043081e904880146e0332860e44f152ce
David Petroff <david.petroff@my.tccd.edu>
2025-10-02 19:34:06 -0500

    lab2

--- labs/David Petroff Lab2 COSC 1436 Fall/David Petroff Lab2 COSC 1436 Fall/David Petroff Lab2 COSC 1436 Fall.cpp
+++ labs/David Petroff Lab2 COSC 1436 Fall/David Petroff Lab2 COSC 1436 Fall/David Petroff Lab2 COSC 1436 Fall.cpp
@@ -14,28 +14,38 @@ int main()
     float loan;
     float interest;
     float payment;
-
-    std::cout << "Please enter a loan amount: " << endl; //limit it between 1- 1000$
-    std::cin >> loan;
-    if (loan < 1 || loan > 1000)
-    {
-        std::cout << "ERROR: Loan amount must be between $1 - $1000" << endl;
-        loan = 1;
+    do {
+        std::cout << "Please enter a loan amount: " << endl; //limit it between 1- 1000$
+        std::cin >> loan;
+        if (loan < 1 || loan > 1000)
+        {
+            std::cout << "ERROR: Loan amount must be between $1 - $1000" << endl;
+            loan = 1;
+        }
+        while (loan < 1 || loan > 1000);
     }
 
-    std::cout << "Please enter an interest rate(%): " << endl;
-    std::cin >> interest;
-    if (interest > 1.0 || interest < 100)
-    {
-        std::cout << "ERROR: interest rate must be between 1.0% - 100.0%" << endl;
-        interest = 1.0;
+    do {
+        std::cout << "Please enter an interest rate(%): " << endl;
+        std::cin >> interest;
+        if (interest > 1.0 || interest < 100)
+        {
+            std::cout << "ERROR: interest rate must be between 1.0% - 100.0%" << endl;
+            interest = 1.0;
+        }
+        while (interest > 1.0 || interest < 100);
     }
-    std::cout << "How much do you want to pay each month?: " << endl;        //no negatives allowed 
-    std::cin >> payment;
-    if (payment < 0 || payment > loan)
-        std::cout << "ERROR: payment must be between $0 - loan amount" << endl;
 
+    do {
+        std::cout << "How much do you want to pay each month?: " << endl;        //no negatives allowed 
+        std::cin >> payment;
+        if (payment < 0 || payment > loan)
+        {
+            std::cout << "ERROR: payment must be between $0 - loan amount" << endl;
+            payment = 1;
+        }
+        while (payment < 0 || payment > loan);
+    }
                                                                             //display loan table
 }
 
-return 0;
